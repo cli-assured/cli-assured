0c3f3793b59b68c09cb0bed3fb1a669ee13488cc
Peter Palaga <ppalaga@redhat.com>
2026-06-18 17:33:00 +0200

    Document CommandSpec.command()

---
+++ cli-assured-tests-java21/src/test/java/org/cliassured/test/j21/docs/CommandSpecCommandTest.java
@@ -0,0 +1,56 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 CLI Assured contributors as indicated by the @author tags
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.cliassured.test.j21.docs;
+
+import java.util.stream.IntStream;
+
+// tag::imports[]
+import org.cliassured.CliAssured;
+import org.cliassured.CommandSpec;
+// end::imports[]
+import org.junit.jupiter.api.Test;
+
+public class CommandSpecCommandTest {
+
+    @Test
+    void command() {
+        // @formatter:off
+        // tag::command[]
+        CliAssured
+            .command("echo", "Hello", "World!")   // Call the echo command with two parameters "Hello" and "World!"
+            .execute()
+            .assertSuccess();
+        // end::command[]
+        // @formatter:on
+    }
+
+    @Test
+    void executableArgs() {
+        // @formatter:off
+        // tag::executableArgs[]
+
+        // Set the executable echo
+        CommandSpec echo = CliAssured.given().executable("echo");
+
+        // Add arguments 1, 2 and 3
+        for (int i = 1; i < 4; i++) {
+            echo = echo.arg(String.valueOf(i));
+            // note that all CLI Assured API methods return an adjusted copy,
+            // so we need to assign the result of echo.arg(...) back to echo
+        }
+
+        echo
+            .then()
+                .stdout()
+                    .log()
+                    .hasLines("1 2 3") // ensure 1 2 3 is printed
+                    .hasLineCount(1)   // ... and nothing else
+                .execute()
+                .assertSuccess();
+        // end::executableArgs[]
+        // @formatter:on
+    }
+
+}
--- docs/modules/ROOT/examples/j21/HelloWorldTest.java
+++ docs/modules/ROOT/examples/j21/HelloWorldTest.java
@@ -16,17 +16,17 @@ public class HelloWorldTest {
         // @formatter:off
         // tag::snippet[]
         CliAssured
-                .command("echo", "Hello World!")   // Call the echo command with parameter "Hello World!"
-                .then()
-                    .stdout()
-                        .hasLines("Hello World!")  // Fail if there is no line equal to "Hello World!" in stdout
-                        .hasLinesMatching("Hello .*") // Fail if there is no line matching the given regular expression
-                        .doesNotHaveLines("foo", "bar") // Fail if foo or bar occur in stdout
-                        .hasLineCount(1)          // Fail unless stdout has exactly one line
-                                                  // Possibly more asserts here
-                        .log()                    // Log everything received from stdout
-                .execute()                        // Start the echo process on the operating system
-                .assertSuccess();                 // Fail if "Hello World!" was not in stdout or exit code was not 0
+            .command("echo", "Hello World!")   // Call the echo command with parameter "Hello World!"
+            .then()
+                .stdout()
+                    .hasLines("Hello World!")  // Fail if there is no line equal to "Hello World!" in stdout
+                    .hasLinesMatching("Hello .*") // Fail if there is no line matching the given regular expression
+                    .doesNotHaveLinesContaining("foo", "bar") // Fail if foo or bar occur in stdout
+                    .hasLineCount(1)          // Fail unless stdout has exactly one line
+                                              // Possibly more asserts here
+                    .log()                    // Log everything received from stdout
+            .execute()                        // Start the echo process on the operating system
+            .assertSuccess();                 // Fail if "Hello World!" was not in stdout or exit code was not 0
         // end::snippet[]
         // @formatter:on
     }
