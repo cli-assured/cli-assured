5217e734d50586a583fbfff5318799f9e0c17837
Peter Palaga <ppalaga@redhat.com>
2026-06-22 15:30:16 +0200

    Document org.cliassured.CommandSpec.autoClose*(*)

---
+++ cli-assured-tests-java21/src/test/java/org/cliassured/test/j21/docs/AutoCloseForciblyTest.java
@@ -0,0 +1,41 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 CLI Assured contributors as indicated by the @author tags
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.cliassured.test.j21.docs;
+
+// tag::imports[]
+import java.time.Duration;
+import org.cliassured.CliAssured;
+import org.cliassured.CommandProcess;
+// end::imports[]
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+public class AutoCloseForciblyTest {
+
+    @Test
+    @DisabledOnOs(OS.WINDOWS)
+    void autoCloseForcibly() {
+        // @formatter:off
+        // tag::snippet[]
+        // Start a long-running process using try-with-resources
+        try (CommandProcess proc = CliAssured
+                .command("sleep", "60")
+                // Use forcible kill (SIGKILL) when closing instead of the default graceful kill (SIGTERM)
+                .autoCloseForcibly()
+                // Wait up to 5 seconds for the process to terminate after closing
+                .autoCloseTimeout(Duration.ofSeconds(5))
+                .then()
+                    .exitCodeIsAnyOf(137, 143) // SIGKILL=137, SIGTERM=143
+                .start()) {
+
+            // Do some work while the process is running...
+
+        } // The process is forcibly killed here when the try block exits
+        // end::snippet[]
+        // @formatter:on
+    }
+
+}
